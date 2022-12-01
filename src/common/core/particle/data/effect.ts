ecaf209f942d8c95a5ff816fcdb23c0618ed5a59
NonSpicyBurrito <burritoyangster@gmail.com>
2022-12-01 13:27:09 +0800

    Fixed particle effect typing

--- src/common/core/particle/data/effect.ts
+++ src/common/core/particle/data/effect.ts
@@ -11,8 +11,8 @@ export type ParticleDataTransform = Record<
     Partial<
         Record<
             | 'c'
-            | `${'r' | 'sinr' | 'cosr'}${1 | 2 | 3 | 4}`
-            | `${'x' | 'y'}${1 | 2 | 3 | 4}`,
+            | `${'x' | 'y'}${1 | 2 | 3 | 4}`
+            | `${'r' | 'sinr' | 'cosr'}${1 | 2 | 3 | 4 | 5 | 6 | 7 | 8}`,
             number
         >
     >
@@ -55,5 +55,8 @@ export type ParticleDataGroupParticleProperty = {
 }
 
 export type ParticleDataGroupParticlePropertyExpression = Partial<
-    Record<'c' | `${'r' | 'sinr' | 'cosr'}${1 | 2 | 3 | 4}`, number>
+    Record<
+        'c' | `${'r' | 'sinr' | 'cosr'}${1 | 2 | 3 | 4 | 5 | 6 | 7 | 8}`,
+        number
+    >
 >
