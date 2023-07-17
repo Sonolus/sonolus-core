05e3dafffba4374e31e76f17ce575813ebc7e78c
NonSpicyBurrito <burritoyangster@gmail.com>
2023-07-17 15:09:26 +0800

    Added missing archetype callback export

--- src/common/core/engine/playData/index.ts
+++ src/common/core/engine/playData/index.ts
@@ -1,3 +1,3 @@
-export { EnginePlayDataArchetype } from './archetype'
+export { EnginePlayDataArchetype, EnginePlayDataArchetypeCallback } from './archetype'
 export { EnginePlayDataBucket, EnginePlayDataBucketSprite } from './bucket'
 export { EnginePlayData } from './playData'
