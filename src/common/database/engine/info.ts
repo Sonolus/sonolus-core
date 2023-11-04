54cbcac5297b7c224290dd20789100aa4b95c625
Burrito <burritoyangster@gmail.com>
2023-11-04 09:29:02 +0800

    Updated engine version

--- src/common/database/engine/info.ts
+++ src/common/database/engine/info.ts
@@ -4,7 +4,7 @@ import { LocalizationText } from '../localization'
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 export interface EngineInfo {
     name: string
-    version: 10
+    version: 11
     title: LocalizationText
     subtitle: LocalizationText
     author: LocalizationText
