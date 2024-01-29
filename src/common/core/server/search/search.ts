1ef5cb155eb526eb8c7ad132c0a5b00971246d4a
Burrito <burritoyangster@gmail.com>
2024-01-29 21:16:50 +0800

    Added title and icon to search

--- src/common/core/server/search/search.ts
+++ src/common/core/server/search/search.ts
@@ -1,5 +1,10 @@
+import { Icon } from '../../icon'
+import { Text } from '../../texts'
 import { SearchOption } from './option'
 
 export type Search = {
+    type: string
+    title: Text | (string & {})
+    icon?: Icon
     options: SearchOption[]
 }
