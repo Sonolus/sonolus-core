5ba36c094482e1ed9e120b1911e001587a3366db
Burrito <burritoyangster@gmail.com>
2024-04-10 06:08:33 +0800

    Updated packages and project setup

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import eslint from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import tsEslint from 'typescript-eslint'
+
+export default tsEslint.config(
+    {
+        ignores: ['**/*.*', '!src/**/*.*'],
+    },
+
+    eslint.configs.recommended,
+
+    ...tsEslint.configs.strictTypeChecked,
+    ...tsEslint.configs.stylisticTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules: {
+            '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+            '@typescript-eslint/explicit-module-boundary-types': 'error',
+            '@typescript-eslint/switch-exhaustiveness-check': 'error',
+            '@typescript-eslint/ban-types': [
+                'error',
+                {
+                    types: {
+                        '{}': false,
+                    },
+                    extendDefaults: true,
+                },
+            ],
+        },
+    },
+
+    eslintConfigPrettier,
+)
