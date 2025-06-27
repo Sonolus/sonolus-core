185dae65d75d51118b5b7ae704f8ce4357df6ffe
Burrito <burritoyangster@gmail.com>
2025-06-27 18:55:17 +0800

    Support Sonolus 0.9.1 (#56)

--- src/common/core/service/user-profile.ts
+++ src/common/core/service/user-profile.ts
@@ -2,8 +2,12 @@ export type ServiceUserProfile = {
     id: ServiceUserId
     handle: string
     name: string
+    avatarType: string
+    avatarForegroundType: string
     avatarForegroundColor: string
+    avatarBackgroundType: string
     avatarBackgroundColor: string
+    bannerType: string
     aboutMe: string
     favorites: string[]
 }
