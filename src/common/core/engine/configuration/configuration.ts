fcd8c13e32a4052eb24802d91201a135495bad63
NonSpicyBurrito <burritoyangster@gmail.com>
2025-12-30 18:50:59 +0800

    Added keyed replay options

--- src/common/core/engine/configuration/configuration.ts
+++ src/common/core/engine/configuration/configuration.ts
@@ -1,7 +1,9 @@
+import { Text } from '../../text.js'
 import { EngineConfigurationOption } from './option.js'
 import { EngineConfigurationUI } from './ui.js'
 
 export type EngineConfiguration = {
     options: EngineConfigurationOption[]
     ui: EngineConfigurationUI
+    replayFallbackOptionNames?: (Text | (string & {}))[]
 }
