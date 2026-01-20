083cdcc889d99bdcd30253a3746d20269939110c
NonSpicyBurrito <burritoyangster@gmail.com>
2026-01-21 00:21:28 +0800

    Updated packages

--- src/node/core/resource/decompress.ts
+++ src/node/core/resource/decompress.ts
@@ -1,11 +1,11 @@
 import { promisify } from 'node:util'
-import { unzip, unzipSync } from 'node:zlib'
+import { InputType, unzip, unzipSync } from 'node:zlib'
 
 const unzipPromise = promisify(unzip)
 
-export const decompress = async <T>(data: Buffer): Promise<T> =>
+export const decompress = async <T>(data: InputType): Promise<T> =>
     JSON.parse((await unzipPromise(data)).toString()) as never
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-parameters
-export const decompressSync = <T>(data: Buffer): T =>
+export const decompressSync = <T>(data: InputType): T =>
     JSON.parse(unzipSync(data).toString()) as never
--- src/node/core/resource/hash.ts
+++ src/node/core/resource/hash.ts
@@ -1,3 +1,3 @@
-import { createHash } from 'node:crypto'
+import { BinaryLike, createHash } from 'node:crypto'
 
-export const hash = (buffer: Buffer): string => createHash('sha1').update(buffer).digest('hex')
+export const hash = (buffer: BinaryLike): string => createHash('sha1').update(buffer).digest('hex')
