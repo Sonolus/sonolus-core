19eaa2c1f566adff37f1477452c49c373c743c20
NonSpicyBurrito <burritoyangster@gmail.com>
2026-03-14 02:30:11 +0800

    Added title to server item info and list

--- src/common/core/server/items/info.ts
+++ src/common/core/server/items/info.ts
@@ -1,8 +1,10 @@
 import { Srl } from '../../srl.js'
+import { Text } from '../../text.js'
 import { ServerForm } from '../form.js'
 import { ServerItemSection } from './section.js'
 
 export type ServerItemInfo = {
+    title?: Text | (string & {})
     creates?: ServerForm[]
     searches?: ServerForm[]
     quickSearchValues?: string
