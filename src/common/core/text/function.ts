b276c05dfe472b86718dd174f07a31548dba6749
NonSpicyBurrito <burritoyangster@gmail.com>
2026-05-21 05:21:34 +0800

    Added text function

---
+++ src/common/core/text/function.ts
@@ -0,0 +1,3 @@
+export const Escape = '##' as const
+export const TimeFull = '##TIME_FULL' as const
+export const TimeRelative = '##TIME_RELATIVE' as const
--- src/common/core/text/index.ts
+++ src/common/core/text/index.ts
@@ -1,3 +1,7 @@
 export * as Text from './text.js'
 
 export type Text = (typeof import('./text.js'))[keyof typeof import('./text.js')]
+
+export * as TextFunction from './function.js'
+
+export type TextFunction = (typeof import('./function.js'))[keyof typeof import('./function.js')]
